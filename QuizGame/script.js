177f090850dd917862539e2f097daf4e2c3e9e23
codekhush <khushalkhode001@gmail.com>
2024-09-21 00:18:19 +0530

    Add files via upload

---
+++ QuizGame/script.js
@@ -0,0 +1,12 @@
+// Linking: question, options
+let QuestionS = document.querySelector('#question');
+let Opt1 = document.querySelector('#opt1');
+let Opt2 = document.querySelector('#opt2');
+let Opt3 = document.querySelector('#opt3');
+let Opt4 = document.querySelector('#opt4');
+let optionContainer = document.querySelector('.opt_container');
+let Next = document.querySelector('#next');
+// Question and Answers Array Traversing: 
+
+// Creating Arrays:
+ 
